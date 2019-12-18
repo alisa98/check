1100381423d2fb2941fd209716f9e77b1aa2d414
Alisa Utlik <alisa.utlik@gmail.com>
2019-12-18 22:15:02 +0300

    add project

---
+++ src/main/java/bsu/ws/shop/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package bsu.ws.shop.repos;
+
+import bsu.ws.shop.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
